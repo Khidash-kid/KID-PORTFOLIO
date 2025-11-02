604dbccf959b4b983045175b0916936cc6fac646
Khidash-kid <khidashshah10@gmail.com>
2025-11-02 15:12:34 +0530

    added successfully

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/PortFolio/',
+  base:'/',
   build: {
     outDir: 'dist', // This tells Vite to output files to 'dist'
   },
